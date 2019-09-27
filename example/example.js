cb4cf4bbe4449c7e9b6a5c21dd886167c3e55944
Miguel Mota <miguelmota2@gmail.com>
2019-09-26 22:55:43 -0700

    Lint

--- example/example.js
+++ example/example.js
@@ -1,31 +1,29 @@
-#!/usr/bin/env node
+var BigRedButton = require('../')
 
-var BigRedButton = require('../');
-
-var bigRedButtons = [];
+var bigRedButtons = []
 
 for (var i = 0; i < BigRedButton.deviceCount(); i++) {
-  console.log('opening BigRedButton', i);
+  console.log('opening BigRedButton', i)
 
-  bigRedButtons.push(new BigRedButton.BigRedButton(i));
+  bigRedButtons.push(new BigRedButton.BigRedButton(i))
 
   bigRedButtons[i].on('buttonPressed', function () {
-    console.log('Button pressed');
-  });
+    console.log('Button pressed')
+  })
 
   bigRedButtons[i].on('buttonReleased', function () {
-    console.log('Button released');
-  });
+    console.log('Button released')
+  })
 
   bigRedButtons[i].on('lidRaised', function () {
-    console.log('Lid raised');
-  });
+    console.log('Lid raised')
+  })
 
   bigRedButtons[i].on('lidClosed', function () {
-    console.log('Lid closed');
-  });
+    console.log('Lid closed')
+  })
 
   bigRedButtons[i].on('error', function (error) {
-    console.error(error);
-  });
+    console.error(error)
+  })
 }
